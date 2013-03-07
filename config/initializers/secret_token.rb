31a3b1d83ee70cfce662ff586f3c91b1e62d1c21
Ryosuke Shinozaki <ryshinoz@gmail.com>
2013-03-07 18:06:28 +0900

    New

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toeicfive::Application.config.secret_key_base = '1988f3a20b0b392d3b8f80d295918921dd5308bb4bf382a7c7261cbe251967f5bdbba8633a0b8fe9d5a1ac5e221adabea56bcd434fd11d39eb091e2b291a48f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Toeicfive::Application.config.session_store :encrypted_cookie_store, key: '_toeicfive_session'
