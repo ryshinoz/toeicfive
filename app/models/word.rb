5d430a41aa88d44361d4a066d6e8062ad508c8c1
nakamura shinichirou <naka5313@gmail.com>
2013-03-10 12:29:40 +0900

    random_word method update. test case add.

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,8 +1,14 @@
 class Word < ActiveRecord::Base
+  scope :not_complete,->{where('is_complete =?', false)}
 
   def self.random_words
-    #Word.where('is_complete = ?', false)..limit(1)
-    Word.where('is_complete = ?', false).offset(rand(Word.count)).limit(5)
+    @words = Word.not_complete
+    count = Word.not_complete.count
+    if count > 20
+      @words.offset(rand(count - 20))
+    end 
+    @words.limit 20
+    #Word.not_complete.offset(rand(Word.not_complete.count)).limit(20)
   end
 
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,15 +1,39 @@
+#coding: utf-8
+
 require 'spec_helper'
 
 describe Word do
   fixtures :words
-  context "aaa" do
-    before do
-      @words = Word.random_words
-      p @words
-    end
-    it do
-      @words.should_not be_nil
-      @words.size.should eql(1)
+  describe "random_words method test"do
+    context "random_words method return value " do
+      before do
+        @words = Word.random_words
+        p @words
+      end
+      it "return value not nil" do
+        @words.should_not be_nil
+      end 
+      it "return value size check" do
+        @words.size.should eql(20)
+      end
+    end 
+    context "some data is_complete column update" do
+      before do
+        @word = Word.where('name =?','Nakamura').first
+        @word.is_complete = true
+        @word.save
+
+        @word = Word.where('name = ?','Shinozaki').first
+        @word.is_complete = true
+        @word.save
+
+        @word = Word.where('name = ?','Irokawa').first
+        @word.is_complete = true
+        @word.save
+      end
+      it "return value size check" do
+         Word.random_words.size.should eql(19)
+      end
     end
   end
 end
